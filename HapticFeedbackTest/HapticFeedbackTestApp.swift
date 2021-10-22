2bce009713452011589d81d8849cd1f223dff96e
Vince14Genius <vince14genius@icloud.com>
2021-10-22 16:52:59 -0700

    Initial Commit

---
+++ HapticFeedbackTest/HapticFeedbackTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticFeedbackTestApp.swift
+//  HapticFeedbackTest
+//
+//  Created by Vincent C. on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct HapticFeedbackTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
